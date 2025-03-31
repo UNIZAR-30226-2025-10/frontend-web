5b0b4587deec8bc437c9222f593d85c8c60d85d3
martaibnz <martaibaallo@gmail.com>
2025-04-01 01:13:00 +0200

    z

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
-import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import { Router } from '@angular/router';
+import { TokenService } from './services/token.service';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { Component, LOCALE_ID, OnInit } from '@angular/core';
+
+registerLocaleData(localeEs);
 
 @Component ({
 selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-
+  styleUrl: './app.component.css',
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' } // Establecer idioma por defecto como español
+  ]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Noizz';
+
+  constructor(private router: Router, private tokenService: TokenService) {}
+
+  ngOnInit() {
+    const token = this.tokenService.getToken();
+    console.log('token en app:', token);
+  
+    setTimeout(() => {
+      if (token && this.router.url === '/login') {  
+        this.router.navigate(['/home/home']);
+      }
+    }, 0); 
+  }
 }
-
-
